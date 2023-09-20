8895592df946858427ae57f1f46bac0fee1b0c66
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-09-19 20:02:31 -0400

    class changes

--- src/components/DictionarySelects.jsx
+++ src/components/DictionarySelects.jsx
@@ -2,40 +2,24 @@ import {
   Chip,
   Stack,
 } from '@mui/material';
-import { useEffect, useState } from 'react';
-// const [dictionaries, setDictionaries] = useState([undefined]]);
-//const [selectedDictionary, setSelectedDictionary] = useState(undefined);
+import { useDictionary } from '../utils/DictionaryContext';
 
-function DictionarySelects(props) {
 
-  const [selectedDictionary, setSelectedDictionary] = useState(undefined)
-  const [dictionaries, setDictionaries] = useState([]);
+function DictionarySelects() {
 
-  useEffect(() => {
-    fetch('https://1rnoszgn46.execute-api.us-east-1.amazonaws.com/dictionaries')
-      .then((data) => data.json())
-      .then((data) => {
-        setDictionaries(data);
-        if (data.length > 0) {
-          props.setActiveDictionary(data[0]);
-          setSelectedDictionary(data[0])
-        }
-      })
-      .catch((e) => console.log(e));
-  }, []);
-
+  const { state, active, setActive} = useDictionary();
+  
   return (
     <div>
       <Stack direction='column'>
-        {dictionaries &&
-          dictionaries.map((d, index) => (
-            <Chip  
-            color={d._id !== selectedDictionary._id  ? "primary" : "secondary"}
-             label={d.title}
+        {state &&
+          state.map((d, index) => (
+            <Chip
+              color={d._id !== active._id ? "primary" : "secondary"}
+              label={d.title}
               key={index}
               onClick={() => {
-                setSelectedDictionary(d)
-                props.setActiveDictionary(d);
+                setActive(d);
               }}
             />
           ))}
